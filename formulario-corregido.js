47c387ff38ad1b0c2a3c60629949248e0696efd2
egarcode95 <egarcode95@gmail.com>
2023-08-23 18:16:42 -0600

    creación de rama

---
+++ formulario-corregido.js
@@ -0,0 +1,78 @@
+/*  
+Agregué los puntos y comas al final de las lineas que lo necesitaban
+hay una variable que se repite, entonces la reasigne
+*/ 
+
+var formulario = document.querySelector("#form");
+
+formulario.onsubmit = function(e) {
+  e.preventDefault();
+  
+  var n = formulario.elements[0];
+  var edadElem = formulario.elements[1];
+  var na = formulario.elements[2];
+
+  var nombre = n.value;
+  var edad = edadElem.value;
+
+  var i = na.selectedIndex;
+  var nacionalidad = na.options[i].value;
+
+  console.log(nombre, edad);
+  console.log(nacionalidad);
+
+  if (nombre.length === 0) {
+    n.classList.add("error");
+  }
+
+  if (edad < 18 || edad > 120) {
+    edadElem.classList.add("error");
+  }
+
+  if (nombre.length > 0 && edad > 18 && edad < 120) {
+    agregarInvitado(nombre, edad, nacionalidad);
+  }
+};
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina";
+  } else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana";
+  } else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana";
+  } else if (nacionalidad === "per") {
+    nacionalidad = "Peruana";
+  }
+
+  var lista = document.getElementById("lista-de-invitados");
+  var elementoLista = document.createElement("div");
+  elementoLista.classList.add("elemento-lista");
+  lista.appendChild(elementoLista);
+
+  function crearElemento(descripcion, valor) {
+    var spanNombre = document.createElement("span");
+    var inputNombre = document.createElement("input");
+    var espacio = document.createElement("br");
+    spanNombre.textContent = descripcion + ": ";
+    inputNombre.value = valor;
+    elementoLista.appendChild(spanNombre);
+    elementoLista.appendChild(inputNombre);
+    elementoLista.appendChild(espacio);
+  }
+
+  crearElemento("Nombre", nombre);
+  crearElemento("Edad", edad);
+  crearElemento("Nacionalidad", nacionalidad);
+
+  var botonBorrar = document.createElement("button");
+  botonBorrar.textContent = "Eliminar invitado";
+  botonBorrar.id = "boton-borrar";
+  var corteLinea = document.createElement("br");
+  elementoLista.appendChild(corteLinea);
+  elementoLista.appendChild(botonBorrar);
+
+  botonBorrar.onclick = function() {
+    botonBorrar.parentNode.remove();
+  };
+}
